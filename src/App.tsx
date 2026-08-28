422b0095f39116819b64244807da61d32925b224
jklepp <jklepp@gmail.com>
2026-08-27 22:57:24 -0400

    Initial Vite React TypeScript setup

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>My First React App</h1>
+      <p>Built from scratch with Vite, React, and TypeScript.</p>
+    </div>
+  );
+}
+
+export default App;
